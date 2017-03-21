10ed542d77aa9126e8865a09bf9db3a8d9f4b3ee
Ioannis Apostolidis <i.apostolidis@encodegroup.com>
2017-03-21 17:50:48 +0200

    react-bootstrap and header

--- frontend/app/components/header/Header.jsx
+++ frontend/app/components/header/Header.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import { Navbar, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
 
 export default class Header extends React.Component {
-  state = { isLoggedIn: undefined }
+  state = {}
 
   render() {
     return (
-      <nav className="navbar navbar-default navbar-fixed-top">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="/">
-              Wiregoose
-            </a>
-          </div>
-        </div>
-      </nav>
+      <Navbar collapseOnSelect fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">Wiregoose</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            {/* <NavItem eventKey={1} href="#">Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>*/}
+            <NavDropdown
+              eventKey={3}
+              title={<FontAwesome name="bars" />}
+              id="basic-nav-dropdown"
+              noCaret
+            >
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              {/* <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>*/}
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
